a1ed2956546b642dd1c6160c44492e299e952aa1
xiesh123 <125528296+xiesh123@users.noreply.github.com>
2023-07-14 15:33:31 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("JavaScript file loaded");
+
+$(document).ready(function() {
+  var myName = "Sharon Xie";
+
+  // Display the name on the loading page
+  $("#loader .loader-content h1").text(myName);
+
+  // Transition to the next page after 3 seconds
+  setTimeout(function() {
+    window.location.href = "index.html";
+  }, 3000);
+});
+
+
+
+
+
